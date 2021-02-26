d2264123ae43424b196accfa91f547d33269e559
Berkay Ersever <berkayersever@sabanciuniv.edu>
2021-02-26 14:45:47 +0300

    Update logs.js

--- routes/logs.js
+++ routes/logs.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
-});
+});*/
 
 router.get('/', (req, res, next) => {
     const fs = require("fs");
-    let data = JSON.parse(fs.readFileSync('dummyEvents.json', 'utf-8'));
+    let data = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'public', 'events', 'dummyEvents.json'), 'utf-8'));
     res.status(200).json(data);
 });
 
