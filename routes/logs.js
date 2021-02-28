aa65b8bd290ef8854f895242cb61c4ef08d28a63
Berkay Ersever <berkayersever@sabanciuniv.edu>
2021-03-01 02:39:54 +0300

    Update logs.js

--- routes/logs.js
+++ routes/logs.js
@@ -7,10 +7,20 @@ const path = require('path');
     res.render('index', { title: 'Express' });
 });*/
 
-router.get('/', (req, res, next) => {
+/*router.get('/', (req, res, next) => {
     const fs = require("fs");
-    let data = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'public', 'events', 'dummyEvents.json'), 'utf-8'));
+    let pathTest = path.relative(__dirname, '../public/events/dummyEvents.json');
+    console.log(pathTest);
+    console.log(path.join('..', 'public', 'events', 'dummyEvents.json'));
+    // let data = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'public', 'events', 'dummyEvents.json'), 'utf-8'));
+    // let data = JSON.parse(fs.readFileSync(path.resolve(__dirname, path.join('..', 'public', 'events', 'dummyEvents.json')), 'utf-8'));
+    let data = require(path.resolve(__dirname, path.join('..', 'public', 'events', 'dummyEvents.json')));
     res.status(200).json(data);
+});*/
+
+router.get('/', (req, res, next) => {
+    let data = JSON.parse(fs.readFileSync(path.resolve(__dirname, path.join('..', 'public', 'events', 'dummyEvents.json')), 'utf-8'));
+    res.render('logs', { events: data });
 });
 
 module.exports = router;
