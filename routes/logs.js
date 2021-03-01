a684993a9dd17f4dd3cc76752b1c19e1de7d3332
Berkay Ersever <berkayersever@sabanciuniv.edu>
2021-03-01 13:27:17 +0300

    Update logs.js

--- routes/logs.js
+++ routes/logs.js
@@ -19,8 +19,8 @@ const path = require('path');
 });*/
 
 router.get('/', (req, res, next) => {
-    let data = JSON.parse(fs.readFileSync(path.resolve(__dirname, path.join('..', 'public', 'events', 'dummyEvents.json')), 'utf-8'));
-    res.render('logs', { title: 'Logs', events: data });
+    let logs = JSON.parse(fs.readFileSync(path.resolve(__dirname, path.join('..', 'public', 'events', 'dummyEvents.json')), 'utf-8'));
+    res.render('logs', { title: 'Logs', event_list: logs });
 });
 
 module.exports = router;
