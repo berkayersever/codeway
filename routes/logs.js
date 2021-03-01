4d5938ee660a4a6269c12e64d695e82ad22608fc
Berkay Ersever <berkayersever@sabanciuniv.edu>
2021-03-02 01:20:55 +0300

    Update logs.js

--- routes/logs.js
+++ routes/logs.js
@@ -2,22 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-/* GET home page. */
-/*router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});*/
-
-/*router.get('/', (req, res, next) => {
-    const fs = require("fs");
-    let pathTest = path.relative(__dirname, '../public/events/dummyEvents.json');
-    console.log(pathTest);
-    console.log(path.join('..', 'public', 'events', 'dummyEvents.json'));
-    // let data = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'public', 'events', 'dummyEvents.json'), 'utf-8'));
-    // let data = JSON.parse(fs.readFileSync(path.resolve(__dirname, path.join('..', 'public', 'events', 'dummyEvents.json')), 'utf-8'));
-    let data = require(path.resolve(__dirname, path.join('..', 'public', 'events', 'dummyEvents.json')));
-    res.status(200).json(data);
-});*/
-
+/* GET logs page. */
 router.get('/', (req, res, next) => {
     const fs = require("fs");
     try {
