aac3828d72f2825fae39b31d2ab078fcdfdf5877
Berkay Ersever <berkayersever@sabanciuniv.edu>
2021-03-09 13:18:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -31,15 +31,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/logs', logsRouter);
 app.use('/users', usersRouter);
-app.use('/publishBatchedMessages', publishRouter);
+app.use('/publish', publishRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
