671f9ffae074dddfe176af6c08f652f613f164d6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2021-03-19 11:58:56 +0300

    Update query.js

--- routes/query.js
+++ routes/query.js
@@ -1,9 +1,6 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 const router = express.Router();
 const bigQuery = require('../lib/source/bigQueryService');
-
-// const jsonParser = bodyParser.json()
 
 /* GET Daily Active Users */
 router.get('/dailyActiveUsers', (req, res, next) => {
